4985b9b7579ca0cbc8128ae66ebb565b9aa339b4
Artem <tradegordeev@gmail.com>
2023-09-19 16:30:50 +0300

    ad

---
+++ Mongo.Services.CouponAPI/Controllers/CouponApiController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mongo.Services.CouponAPI.Data;
+using Mongo.Services.CouponAPI.Model;
+using Mongo.Services.CouponAPI.Model.Dto;
+using Mongo.Services.CouponAPI.Model.Folder;
+using System.Collections;
+
+namespace Mongo.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponApiController : ControllerBase
+    {
+
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+        private IMapper _mapper;
+        public CouponApiController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(u => u.CouponId == id);
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+
+            return _response;
+        }
+
+    }
+}
---
+++ Mongo.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mongo.Services.CouponAPI.Model;
+
+namespace Mongo.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon
+                {
+                    CouponId = 1,
+                    CouponCode = "10Off",
+                    DiscountAmount = 10,
+                    MinAmount = 20
+                },
+                new Coupon
+                {
+                    CouponId = 2,
+                    CouponCode = "10Ogg",
+                    DiscountAmount = 20,
+                    MinAmount = 30
+                }
+            );
+
+        }
+
+    }
+}
